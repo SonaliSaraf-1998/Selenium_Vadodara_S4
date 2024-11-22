5666bb8f7364a4ae2645e391b5c5294f45387e6d
dell <dell@DESKTOP-GKE364G>
2024-11-22 09:51:09 +0530

    new commit

--- com.ShopperStack_Sonali/src/main/java/Shopperstack_GenericUtility/Base_Test.java
+++ com.ShopperStack_Sonali/src/main/java/Shopperstack_GenericUtility/Base_Test.java
@@ -59,12 +59,12 @@ public class Base_Test {
 	public void beforeClass() throws IOException{
 	System.out.println("@BeforeClass");
 	
-	//String browser = fileUtility.readPropertyFile("browserName");
+	String browser = fileUtility.readPropertyFile("browserName");
 	
-	//String url = fileUtility.readPropertyFile("url");
+	String url = fileUtility.readPropertyFile("url");
 	
-String url = System.getProperty("url");
-String browser = System.getProperty("browserName");
+//String url = System.getProperty("url");
+//String browser = System.getProperty("browserName");
 	
 	if(browser.contains("chrome")) {
 		driver=new ChromeDriver();
